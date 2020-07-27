2ed381dbf9f047395900304cfe9c14c890539691
Dhanish Kumar <sdhanishk@gmail.com>
2020-07-28 03:50:45 +0530

    Added config file for apis

---
+++ front-end/src/utils/apiConfig.js
@@ -0,0 +1,10 @@
+const baseUrl = "http://localhost:8080"
+
+const area = 'Asia';
+const location = 'Kolkata';
+
+const apis = {
+    time: `http://worldtimeapi.org/api/timezone/America/${area}/${location}`,
+}
+
+export {apis};
